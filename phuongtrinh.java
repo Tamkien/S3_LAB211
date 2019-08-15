b45a3d6b54de1c94e1c80a10c50985a71bbc880f
Cao Tam Kiên <kiencthe140322@fpt.edu.vn>
2019-08-15 23:54:32 +0700

    interface S50

---
+++ phuongtrinh.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public interface phuongtrinh {
+    List<Float> calculateEquation(float a, float b);
+    List<Float> calculateQuadraticEquation(float a, float b, float c);
+}
